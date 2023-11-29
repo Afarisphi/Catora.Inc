9ee0c9407b58f10063cd73fc36e16e6b3d162a50
Afariphi <agung.afr10@gmail.com>
2023-11-29 08:11:07 +0700

    update

--- back-end/Authentification/register-user.js
+++ back-end/Authentification/register-user.js
@@ -7,19 +7,23 @@ router.post('/register', async (req, res) => {
   try {
     const { username, password } = req.body;
 
+    // Check if the username is already taken
     const userSnapshot = await firestoreConnection.firestore.collection('users').where('username', '==', username).get();
 
     if (!userSnapshot.empty) {
       return res.status(400).json({ error: 'Username is already taken' });
     }
 
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    // Create a new user object
     const newUser = {
       username,
       password: hashedPassword,
     };
 
+    // Save the user to Firestore
     await firestoreConnection.save('users', newUser);
 
     res.status(201).json({ message: 'User registered successfully' });
